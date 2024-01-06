e125ddeba580329a69d020ff425e18a1a5d5fd72
김민찬 <kimminchan@gimminchan-ui-MacBookAir.local>
2024-01-07 01:17:23 +0900

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import {Provider as ReduxProvider} from 'react-redux'
 import {useStore} from './store/useStore'
-import {DndProvider} from 'react-dnd'
-import {HTML5Backend} from 'react-dnd-html5-backend'
 import {BrowserRouter} from 'react-router-dom'
 import RoutesSetup from './routes/RoutesSetup'
 import {AuthProvider, BoardProvider} from './contexts'
